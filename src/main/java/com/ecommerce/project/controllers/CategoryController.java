e6e47f9c2f2edb34722481289d29d2655d091e12
Srikant <srikantkr336@gmail.com>
2026-01-22 23:19:34 +0530

    validation starts here

--- src/main/java/com/ecommerce/project/controllers/CategoryController.java
+++ src/main/java/com/ecommerce/project/controllers/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controllers;
 
 import com.ecommerce.project.models.Category;
 import com.ecommerce.project.services.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully!", HttpStatus.CREATED);
     }
---
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.ecommerce.project.exceptions;
+
+public class MyGlobalExceptionHandler {
+
+}
