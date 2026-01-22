ebb2dcf9e6f57031c837e0a10702122f123adf26
Srikant <srikantkr336@gmail.com>
2026-01-23 00:17:51 +0530

    Added custom Exception Handler -> MethodArgumentNotValidException

--- src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,5 +1,27 @@
 package com.ecommerce.project.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class MyGlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError)err).getField();
+            String fieldMessage = err.getDefaultMessage();
+            response.put(fieldName, fieldMessage);
+        });
+        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+    }
 }
